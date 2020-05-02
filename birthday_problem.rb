a8b20ad1eaffc744e39ee8db931d671da7e75b89
Syed AbuTalib <syedabutalib@github.com>
2020-05-02 02:12:18 -0500

    initial commit

---
+++ birthday_problem.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'set'
+
+def play(people)
+  enumerated_birthdays = Hash.new(0)
+
+  people.times do
+    enumerated_birthdays[rand(365)] += 1
+  end
+
+  enumerated_birthdays.each do |_key, val|
+    return true if val > 1
+  end
+  false
+end
+
+if ARGV[0].nil?
+  rounds = 1000
+  people = 23
+elsif ARGV[1].nil?
+  rounds = ARGV[0].to_i
+  people = 23
+else
+  rounds = ARGV[0].to_i
+  people = ARGV[1].to_i
+end
+
+totals = {
+  'passes' => 0,
+  'fails' => 0
+}
+
+rounds.times do
+  if play(people)
+    totals['passes'] += 1
+  else
+    totals['fails'] += 1
+  end
+end
+
+puts totals
